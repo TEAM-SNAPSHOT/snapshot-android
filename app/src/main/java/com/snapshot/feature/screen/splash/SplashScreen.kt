c4cd1f779c323cb0e47fc2556d8a5f75efe30afc
kmj5004 <kmjmj8769@gmail.com>
2025-05-11 13:35:32 +0900

    feat: color theme, indication

--- app/src/main/java/com/snapshot/feature/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/snapshot/feature/screen/splash/SplashScreen.kt
@@ -1,16 +1,12 @@
 package com.snapshot.feature.screen.splash
 
-import android.window.SplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import com.snapshot.res.modifier.bg_light
+import com.snapshot.res.modifier.ColorTheme
 import kotlinx.coroutines.delay
 
 @Composable
@@ -25,7 +21,7 @@ fun SplashScreen(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(color = bg_light)
+            .background(color = ColorTheme.colors.bg)
     ) {
 
     }
