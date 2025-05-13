02f3e7be42e485f615021f7f717469dbaa29e292
kmj5004 <kmjmj8769@gmail.com>
2025-05-13 09:05:23 +0900

    feat: bottomNavigationBar

---
+++ app/src/main/java/com/snapshot/feature/component/bottomBar/Destination.kt
@@ -0,0 +1,20 @@
+package com.snapshot.feature.component.bottomBar
+
+import androidx.compose.runtime.Composable
+import com.snapshot.R
+import com.snapshot.feature.screen.album.navigation.ALBUM_ROUTE
+import com.snapshot.feature.screen.setting.navigation.SETTING_ROUTE
+
+enum class Destination(
+    private val iconRes: Int,
+    val route: String,
+    val label: String
+) {
+    ALBUM(R.drawable.photo, ALBUM_ROUTE, "앨범"),
+    SETTING(R.drawable.setting, SETTING_ROUTE, "설장");
+
+    @Composable
+    fun getIcon(): Int {
+        return iconRes
+    }
+}
